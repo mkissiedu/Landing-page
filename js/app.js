d6651fb0bbff2cbd44f84627c44c230845275ef9
Mary Kissiedu <marykissiedu@gmail.com>
2023-05-14 13:42:20 -0400

    added navigation

--- js/app.js
+++ js/app.js
@@ -64,7 +64,10 @@ const myFirstSection = document.querySelector('#section1');
 const mySecondSection = document.querySelector('#section2');
 const myThirdSection = document.querySelector('#section3');
 const myFourthSection = document.createElement('section');
+
 const mainDocument = document.querySelector('main');
+const navBar = document.querySelector('#navbar__list');
+
 myFirstSection.innerHTML = `<div class="landing__container">
 <h2>Section 1</h2>
 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.</p>
@@ -84,12 +87,23 @@ myThirdSection.innerHTML = `<div class="landing__container">
 <p>Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.</p>
 </div>`;
 
-myFourthSection.innerHTML = `<div class="landing__container">
+myFourthSection.innerHTML = `
+<div class="landing__container">
 <h2>Section 4</h2>
 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.</p>
 
 <p>Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.</p>
-</div>`;
+</div>
+`;
+
 
 mainDocument.appendChild(myFourthSection);
+const allSections = document.querySelectorAll('section');
+const totalCount = allSections.length;
+
+for (let i = 1; i <= totalCount; i++) {
+    const navElement = document.createElement('li');
+    navElement.innerHTML = `<li> <a href="#section${i}">Section${i}</a></li>`;
+    navBar.appendChild(navElement);
+}
 
