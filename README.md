a50a4faa8620b5a6726c33f229aca666e0db4eca
Mary Kissiedu <marykissiedu@gmail.com>
2023-06-16 22:02:38 -0400

    updating master branch

--- README.md
+++ README.md
@@ -7,3 +7,11 @@
 ## Project-description
 
 This is a landing page site with 4 different sections and interactivity has been added to the site with javascript.
+
+## Usage
+
+To access the Landing page, open up the index.html file using your preferred browser on any device.
+
+## Dependencies
+
+There are currently no dependencies for this project.
--- js/app.js
+++ js/app.js
@@ -1,35 +1,34 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
-*/
+ *
+ */
 
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
-*/
 
 /**
  * Define Global Variables
- * 
-*/
-const myFirstSection = document.querySelector('#section1');
-const mySecondSection = document.querySelector('#section2');
-const myThirdSection = document.querySelector('#section3');
-const myFourthSection = document.createElement('section');
-const mainDocument = document.querySelector('main');
-const navBar = document.querySelector('#navbar__list');
+ *
+ */
+const myFirstSection = document.querySelector("#section1");
+const mySecondSection = document.querySelector("#section2");
+const myThirdSection = document.querySelector("#section3");
+const myFourthSection = document.createElement("section");
+const mainDocument = document.querySelector("main");
+const navBar = document.querySelector("#navbar__list");
 
-// Build menu 
+/**
+ * Dynamically Populate the four sections on the webpage
+ * 
+ */
 myFirstSection.innerHTML = `<div class="section1">
 <h2>Section 1</h2>
 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.</p>
@@ -59,99 +58,48 @@ myFourthSection.innerHTML = `<section id="section4" data-nav="Section 4">
 </div>
 </section>`;
 
-
+//Append the Fourth Section to the Page
 mainDocument.appendChild(myFourthSection);
 
-// build the nav
-const allSections = document.querySelectorAll('section');
+// Dynamically buidl the navigation bar
+const allSections = document.querySelectorAll("section");
 const totalCount = allSections.length;
-
 for (let i = 1; i < totalCount; i++) {
-    const navElement = document.createElement('li');
-    navElement.innerHTML = `<li class = "menu__link" id ="section${i}"> <a href="#section${i}">Section${i}</a></li>`;
-    navBar.appendChild(navElement);
+  const navElement = document.createElement('li');
+  navElement.innerHTML = `<li class = "menu__link" id ="Section ${i}"> <a href="#section${i}">Section ${i}</a></li>`;
+  navBar.appendChild(navElement);
 }
-
 //add eventlistener on the navbar
-const navSection = document.querySelectorAll('li')
-for (let y = 0; y < navSection.length; y++)
-{
-  console.log(navSection.length + " this is the length of the node list navSection")
-  navSection[y].addEventListener('click',function(event){
-  event.preventDefault();
-  const section1 = document.getElementById('section1')
-  const section2 = document.getElementById('section2')
-  const section3 = document.getElementById('section3')
-  const section4 = document.getElementById('section4')
-  console.log(section1.getAttribute('id') + " this is the id for section1")
-  console.log(myFirstSection.getAttribute('id') + " this is the id for myFirstSection")
-  console.log(navSection[y].getAttribute('id') + " this is the id of the current navsection")
-    if (section1.getAttribute('id')  === myFirstSection.getAttribute('id'))
-      {
-           myFirstSection.scrollIntoView({
-           behavior: 'smooth'
-        });
-     }
-     else if (section2.getAttribute('id') === mySecondSection.getAttribute('id'))
-       {
-          mySecondSection.scrollIntoView({
-          behavior: 'smooth'
-        })
-        }
-      else if (section3.getAttribute('id')  === myThirdSection.getAttribute('id'))
-        {
-          myThirdSection.scrollIntoView({
-          behavior: 'smooth'
-         })
-       }
-       else if (section4.getAttribute('id')  === myFourthSection.getAttribute('id'))
-       {
-          myFourthSection.scrollIntoView({
-          behavior: 'smooth'
-         
-           });
-        }
-
-})
-}; 
-
+const navSection = document.querySelectorAll("a");
+for (let y = 0; y < navSection.length; y++) {
+  navSection[y].addEventListener("click", (e) => {
+    e.preventDefault();
+    const element = document.getElementById(`section${y + 1}`);
+    element.scrollIntoView({ behavior: "smooth", block: "center" }); // scroll to the element, with smooth scrolling
+  });
+}
 
 // Add class 'active' to section when it is near top of viewport
 function makeActive() {
-    for (const section of allSections) {
-      const box = section.getBoundingClientRect();
-      // You can play with the values in the "if" condition to further make it more accurate. 
-      if (box.top <= 150 && box.bottom >= 150) {
-        // Apply active state on the current section and the corresponding Nav link.
-        section.className ='your-active-class';
-      } else {
-        // Remove active state from other section and corresponding Nav link.
-        section.className ='';
-      }
+  for (const section of allSections) {
+    const box = section.getBoundingClientRect();
+    // You can play with the values in the "if" condition to further make it more accurate.
+    if (box.top <= 150 && box.bottom >= 150) {
+      // Apply active state on the current section and the corresponding Nav link.
+      section.className = "your-active-class";
+    } else {
+      // Remove active state from other section and corresponding Nav link.
+      section.className = "";
     }
   }
+}
 
-  // Make sections active
- document.addEventListener("scroll", function() {
- makeActive();
- console.log('The scroll function to make active is called')
-  });
+// Make sections active
+document.addEventListener("scroll", function () {
+  makeActive();
+  console.log("The scroll function to make active is called");
+});
 
-
-  //https://www.w3docs.com/snippets/javascript/how-to-create-smooth-scrolling-when-clicking-an-anchor-link.html
-
-  // mySecondSection.scrollIntoView({
-  //   block: 'start',
-  //   behavior: 'smooth',
-  //   inline: 'start'
-  // });
-  // myThirdSection.scrollIntoView({
-  //   block: 'start',
-  //   behavior: 'smooth',
-  //   inline: 'start'
-  // });
-  // myFourthSection.scrollIntoView({
-  //   block: 'start',
-  //   behavior: 'smooth',
-  //   inline: 'start'
-  // });
+//Reference the below site to look up how to create smooth scrolling and also used the assistance of the tutors.
+//https://www.w3docs.com/snippets/javascript/how-to-create-smooth-scrolling-when-clicking-an-anchor-link.html
+//https://developer.mozilla.org/en-US/docs/Web/API/Element/classList#Methods
